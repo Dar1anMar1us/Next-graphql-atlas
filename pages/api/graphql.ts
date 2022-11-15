770cbc25316fb1f651840588b958d56d1783ac49
DarianChirca <darian.chirca@qmenta.com>
2022-11-16 00:54:36 +0100

    Add type-graphql and create first mock

--- pages/api/graphql.ts
+++ pages/api/graphql.ts
@@ -1,9 +1,32 @@
 import { ApolloServer } from "apollo-server-micro";
 import { MicroRequest } from "apollo-server-micro/dist/types";
 import { ServerResponse, IncomingMessage } from "http";
+import { buildSchema, Resolver, Query, Arg, ObjectType, Field, ID } from "type-graphql";
+import "reflect-metadata";
+
+@ObjectType()
+export class Client {
+    @Field(() => ID)
+    name: string | undefined;
+}
+
+@Resolver(Client)
+export class ClientsResolver {
+    @Query(() => [Client])
+    clients(): Client[] {
+        return [
+            { name: "Darian" },
+            { name: "Chirca" }
+        ]
+    }
+}
+
+const schema = await buildSchema({
+    resolvers: [ClientsResolver]
+})
 
 const server = new ApolloServer({
-
+    schema
 })
 
 export const config = {
